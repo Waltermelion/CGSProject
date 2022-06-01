fa66d168b96bff89754dd415875db90150b26cf8
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-01 14:48:31 +0100

    Jogador quase vai para cima

---
+++ CGS_guibranch/Assets/02_scripts/ReverseGrav.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class ReverseGrav : MonoBehaviour
+{
+    public GameObject player;
+    
+    public float downScaler = 5f;
+
+    private bool isReversed;
+
+    private void FixedUpdate()
+    {
+        if (isReversed)
+        {
+            player.GetComponent<Rigidbody>().AddForce(Vector3.down * downScaler);
+        }
+    }
+
+    void reversegravity()
+    {
+        Debug.Log("REVERSE GRAVITY ACTIVATE");
+        player.GetComponent<Rigidbody>().useGravity = !player.GetComponent<Rigidbody>().useGravity;
+        player.transform.Rotate(180f,0f,0f);
+        isReversed = !isReversed;
+    }
+}
