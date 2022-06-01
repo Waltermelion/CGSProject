06eefcd22474fa2995071a4516ad45392c527cef
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-01 14:58:15 +0100

    Update ReverseGrav.cs

--- CGS_guibranch/Assets/02_scripts/ReverseGrav.cs
+++ CGS_guibranch/Assets/02_scripts/ReverseGrav.cs
@@ -13,15 +13,16 @@ public class ReverseGrav : MonoBehaviour
     {
         if (isReversed)
         {
-            player.GetComponent<Rigidbody>().AddForce(Vector3.down * downScaler);
+            //player.GetComponent<Rigidbody>().AddForce(Vector3.down * downScaler);
         }
     }
 
     void reversegravity()
     {
         Debug.Log("REVERSE GRAVITY ACTIVATE");
-        player.GetComponent<Rigidbody>().useGravity = !player.GetComponent<Rigidbody>().useGravity;
+        //player.GetComponent<Rigidbody>().useGravity = !player.GetComponent<Rigidbody>().useGravity;
         player.transform.Rotate(180f,0f,0f);
         isReversed = !isReversed;
+        Physics.gravity = new Vector3(0f, 10f, 0f); //quase funciona, todos os cubos mudam a gravidade menos o jogador
     }
 }
