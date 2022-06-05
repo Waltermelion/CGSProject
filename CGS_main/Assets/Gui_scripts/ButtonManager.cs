23246dfd5074cc3ddd84effd93a09327c9901b5b
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-05 13:01:30 +0100

    First Timer implementation in button puzzle

--- CGS_main/Assets/Gui_scripts/ButtonManager.cs
+++ CGS_main/Assets/Gui_scripts/ButtonManager.cs
@@ -1,19 +1,32 @@
+using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    //Button Puzzle
     private bool puzzleDone;
     public int ammountBttn;
     public GameObject[] buttonsClicked;
     public Animator doorOpen;
     
+    //Timer
+    public GameObject countDown;
+    private float seconds = 30;
+    private bool timeSeconds = false;
+
+    public GameObject Zero;
     
     private void Start()
     {
         ammountBttn = 10;
+        
+        Zero.SetActive(false);
     }
     private void Update()
     {
+        //Puzzle
         if (!puzzleDone)
         {
             foreach (GameObject gm in buttonsClicked)
@@ -35,5 +48,24 @@ public class ButtonManager : MonoBehaviour
             doorOpen.SetTrigger("AbrirTres");
             Debug.Log("Puzzle Done");
         }
+        
+        //Timer
+        if (timeSeconds == false && seconds > 0)
+        {
+            StartCoroutine(CounterSecond());
+        }
+
+        if (seconds <= 0)
+        {
+                countDown.SetActive(false);
+        }
+    }
+
+    IEnumerator CounterSecond()
+    {
+        timeSeconds = true;
+        yield return new WaitForSeconds(1);
+        seconds -= 1;
+        timeSeconds = false;
     }
 }
