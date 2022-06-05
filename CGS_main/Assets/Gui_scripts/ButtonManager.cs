b05e439054b5b7d4b33be98fabdbaca0b5675af4
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-05 01:07:48 +0100

    seccao 3 praticamente acabada

--- CGS_main/Assets/Gui_scripts/ButtonManager.cs
+++ CGS_main/Assets/Gui_scripts/ButtonManager.cs
@@ -1,18 +1,39 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    public bool[] buttonsClicked;
-    private bool allClicked;
-
+    private bool puzzleDone;
+    public int ammountBttn;
+    public GameObject[] buttonsClicked;
+    public Animator doorOpen;
+    
+    
+    private void Start()
+    {
+        ammountBttn = 10;
+    }
     private void Update()
     {
-        if (allClicked)
+        if (!puzzleDone)
         {
-            
+            foreach (GameObject gm in buttonsClicked)
+            {
+                if (ammountBttn != 0)
+                {
+                    if (gm.GetComponent<ButtonInd>().iHaveBeenClicked && !gm.GetComponent<ButtonInd>().iHaveBeenChecked)
+                    {
+                        ammountBttn--;
+                        gm.GetComponent<ButtonInd>().iHaveBeenChecked = true;
+                    }
+                }
+            }  
+        }
+
+        if (ammountBttn == 0 && !puzzleDone)
+        {
+            puzzleDone = true;
+            doorOpen.SetTrigger("AbrirTres");
+            Debug.Log("Puzzle Done");
         }
     }
 }
