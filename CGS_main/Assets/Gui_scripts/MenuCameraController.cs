d7ecddef608628697b0a1dad0f7458018afabfc3
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-06 20:57:51 +0100

    Main menu camera movement works

---
+++ CGS_main/Assets/Gui_scripts/MenuCameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MenuCameraController : MonoBehaviour
+{
+    public Transform currentMount;
+    public float speedFactor = 0.1f;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentMount.position, speedFactor);
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, speedFactor);
+    }
+
+    public void setMount(Transform newMount)
+    {
+        currentMount = newMount;
+    }
+}
